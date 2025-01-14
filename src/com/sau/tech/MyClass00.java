29f10a304eba46b8f181d6edfa2b4315f58ba218
BanjaratWn <s6752410019@sau.ac.th>
2025-01-14 14:13:14 +0700

    v.2: creat MyClass01, MyClass02 and MyClass00

---
+++ src/com/sau/tech/MyClass00.java
@@ -0,0 +1,9 @@
+package com.sau.tech;
+
+public class MyClass00 {
+    // เมธอด main
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hi, World!");
+    }
+}
