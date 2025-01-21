0e2cbf66bdcb14957bd1a2cbaef8488b5b73dbda
BanjaratWn <s6752410019@sau.ac.th>
2025-01-21 22:26:46 +0700

    Update BMIProject1.java

--- src/com/sau/workshop/BMIProject1.java
+++ src/com/sau/workshop/BMIProject1.java
@@ -2,8 +2,6 @@ package com.sau.workshop;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class BMIProject1 {
@@ -14,24 +12,21 @@ public class BMIProject1 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Input your name : ");
-        empName = sc.next();
-        System.out.print("Input your weight : ");
-        weight = sc.nextDouble();
-        System.out.print("Input your height : ");
-        height = sc.nextDouble();
-
-        System.out.println("--------------------------");
-
-        bmi = weight / height * height;
         System.out.println("++++++++++++++++++++++++++");
         System.out.println("BMI Program by DTI-SAU");
         System.out.println("++++++++++++++++++++++++++");
-        System.out.println("Your name " + empName);
-        System.out.println("Your weight (kg) : " + weight);
-        System.out.println("Your height (m) : " + height);
+        System.out.print("Input your name : ");
+        empName = sc.next();
+        System.out.print("Input your weight (kg) : ");
+        weight = sc.nextDouble();
+        System.out.print("Input your height (m) : ");
+        height = sc.nextDouble();
+
+        bmi = weight / height * height;
+
         System.out.println("++++++++++++++++++++++++++");
-        System.out.println("BMI of  is " + bmi);InputStreamReader input = new InputStreamReader(System.in);
+        System.out.println("BMI of  is " + bmi);
+        InputStreamReader input = new InputStreamReader(System.in);
         System.out.println("++++++++++++++++++++++++++");
 
     }
